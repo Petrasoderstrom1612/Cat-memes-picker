ea5a42d0456926fa37bf5f8520be8b9c361f3b33
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-09-09 11:24:40 +0200

    moved data.js to a separate file

---
+++ data.js
@@ -0,0 +1,20 @@
+export const catsData = [
+    {
+        emotionTags: ["moody"],
+        isGif: false,
+        image: "angry.jpeg",
+        alt: "A cat looking moody",
+    },
+    {
+        emotionTags: ["moody", "insomniac"],
+        isGif: false,
+        image: "angry2.jpeg",
+        alt: "A cat looking moody",
+    },
+    {
+        emotionTags: ["sad"],
+        isGif: true,
+        image: "sad.gif",
+        alt: "A cat looking sad",
+    }
+]
--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-const catsData = [
-    {
-        emotionTags: ["moody"],
-        isGif: false,
-        image: "angry.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody", "insomniac"],
-        isGif: false,
-        image: "angry2.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["sad"],
-        isGif: true,
-        image: "sad.gif",
-        alt: "A cat looking sad",
-    }
-]
+import {catsData} from "./data.js"
 
 const emotionRadiosEl = document.getElementById("emotion-radios-el")
 
